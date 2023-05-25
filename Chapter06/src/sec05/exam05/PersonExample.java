4cd5f6b8287b4f57555fffb25a928730c26d1401
Luncho8 <wkdwlsgh8@naver.com>
2023-05-25 18:19:43 +0900

    master3

---
+++ Chapter06/src/sec05/exam05/PersonExample.java
@@ -0,0 +1,21 @@
+package sec05.exam05;
+
+public class PersonExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person p1= new Person("12345-789123", "홍길동");
+		
+		System.out.println(p1.nation);
+		System.out.println(p1.ssn);
+		System.out.println(p1.name);
+		
+		
+		p1.name = "홍삼원";
+		
+		System.out.println(p1.nation);
+		System.out.println(p1.ssn);
+		System.out.println(p1.name);
+	}
+
+}
